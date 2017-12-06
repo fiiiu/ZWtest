bcfe34aa8240eec3f09d55df44cc737926997861
Alejo Salles <alejosalles@gmail.com>
2017-12-06 13:43:34 -0300

    added hold code deposit

---
+++ hold/contracts/CrowdsaleFactory_v1.sol
@@ -0,0 +1,46 @@
+
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+import './CrowdsaleProperty.sol';
+import './CappedProperty.sol';
+import './WhitelistedProperty.sol';
+import './Crowdsale.sol';
+
+
+contract CrowdsaleFactory {
+  using SafeMath for uint256;
+
+  //address[] public crowdsales; //Alternative
+  event CrowdsaleCreated(address _from, Crowdsale addr);
+  event PropertyAdded(CrowdsaleProperty property);
+
+
+  function createCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, uint256 _cap, address[] _whitelist) public returns(Crowdsale) {
+
+    Crowdsale crowdsale = new Crowdsale(_startTime, _endTime, _rate, _wallet);
+
+    if(_cap > 0){
+      CappedProperty capped = new CappedProperty(_cap);
+      crowdsale.addProperty(capped);
+      PropertyAdded(capped);
+    }
+
+    if(_whitelist.length > 0){
+      WhitelistedProperty whitelisted = new WhitelistedProperty(_whitelist);
+      crowdsale.addProperty(whitelisted);
+      PropertyAdded(whitelisted);
+    }
+
+    //crowdsales.push(crowdsale); //Alternative
+    CrowdsaleCreated(msg.sender, crowdsale);
+    return crowdsale;
+  }
+
+  /* Alternative
+    function getCrowdsaleAddressAtIndex(uint i) constant returns(address c) {
+    return crowdsales[i];
+  }*/
+
+}
---
+++ hold/test/Crowdsale.js
@@ -0,0 +1,132 @@
+// some verbose console.log()s to illustrate what's happening
+// $ truffle build
+// $ truffle test
+// $ should see the first created contract address returned
+
+const CrowdsaleFactory = artifacts.require('CrowdsaleFactory');
+const Crowdsale = artifacts.require('Crowdsale');
+
+
+ // APPROACH B' --test capped
+// var amountBought = 17;
+// contract("CrowdsaleFactory", function(accounts) {
+//   it('should be happy', function() {
+//     return CrowdsaleFactory.deployed().then(function(instance) {
+//       return instance.createCrowdsale(0);
+//     }).then(function(result) {
+//       console.log('w');
+//
+//       // We can loop through result.logs to see if we triggered the Transfer event.
+//       for (var i = 0; i < result.logs.length; i++) {
+//         var log = result.logs[i];
+//         if(log.event == "PropertyAdded") {
+//           console.log('Property added!');
+//         }
+//         if(log.event == "CrowdsaleCreated") {
+//           // We found the event!
+//           console.log('Crowdsale created!');
+//           console.log(log.args);
+//           var crowdsale = Crowdsale.at(log.args.addr);
+//           //console.log(crowdsale.weiRaised.call());
+//           //return crowdsale.weiRaised.call(); //check without call()
+//           crowdsale.buyTokens(accounts[0], {value: amountBought}).then(function() {
+//             //return crowdsale.hasEnded();
+//             return crowdsale.weiRaised.call().then(function(weis) {
+//                   //assert.equal(hasEnded, true)
+//                   console.log("weiRaised: ", weis.toNumber());
+//                   assert.equal(weis.toNumber(), amountBought);
+//                   return crowdsale.hasEnded().then(function (has) {
+//                       console.log("hasEnded: ", has);
+//                   });
+//             });
+//           }).catch(function(e) {
+//             console.log("invalid buy?");
+//             });
+//         }
+//       }
+//     });
+//   });
+// });
+
+
+// //test purchase validity
+// var amountBought = 14;
+// contract("CrowdsaleFactory", function(accounts) {
+//  it('should allow/disallow purchase', function() {
+//    return CrowdsaleFactory.deployed().then(function(instance) {
+//      return instance.createCrowdsale(0, [accounts[4], accounts[1]]);
+//    }).then(function(result) {
+//      console.log('w');
+//      var crowdsale;
+//      // We can loop through result.logs to see if we triggered the Transfer event.
+//      for (var i = 0; i < result.logs.length; i++) {
+//        var log = result.logs[i];
+//        if(log.event == "PropertyAdded") {
+//          console.log('Property added!');
+//        }
+//        if(log.event == "CrowdsaleCreated") {
+//          console.log('Crowdsale created!');
+//          console.log(log.args);
+//          crowdsale = Crowdsale.at(log.args.addr);
+//        }
+//      }
+//      return crowdsale; }).then(function(crowdsale) {
+//         crowdsale.buyTokens(accounts[1], {value: amountBought}).then(function() {
+//         return crowdsale.weiRaised.call().then(function(weis) {
+//          //assert.equal(hasEnded, true)
+//          console.log("weiRaised: ", weis.toNumber());
+//          //assert.equal(weis.toNumber(), amountBought);
+//          // return crowdsale.hasEnded().then(function (has) {
+//          //     console.log("hasEnded: ", has);
+//          //    });
+//             });
+//          }).catch(function(e) {
+//            console.log("invalid buy?");
+//          });
+//        });
+//   });
+// });
+
+//test hasEnded
+var amountToBuy = 14;
+var cap = 14;
+const startTime = Date.now()/1000;
+const endTime = startTime + 100000;
+const rate = 1;
+const wallet = 0x11;
+
+contract("CrowdsaleFactory", function(accounts) {
+ it('should end crowdsale', function() {
+   return CrowdsaleFactory.deployed().then(function(instance) {
+     return instance.createCrowdsale(startTime, endTime, rate, wallet, cap, [accounts[1]], []);
+   }).then(function(result) {
+     console.log('w');
+     var crowdsale;
+     // We can loop through result.logs to see if we triggered the Transfer event.
+     for (var i = 0; i < result.logs.length; i++) {
+       var log = result.logs[i];
+       if(log.event == "PropertyAdded") {
+         console.log('Property added!');
+       }
+       if(log.event == "CrowdsaleCreated") {
+         console.log('Crowdsale created!');
+         console.log(log.args);
+         crowdsale = Crowdsale.at(log.args.addr);
+       }
+     }
+     return crowdsale; }).then(function(crowdsale) {
+        crowdsale.buyTokens(accounts[1], {value: amountToBuy}).then(function() {
+        return crowdsale.hasEnded.call().then(function(ended) {
+         //assert.equal(hasEnded, true)
+         console.log("hasEnded: ", ended);
+         //assert.equal(weis.toNumber(), amountBought);
+         // return crowdsale.hasEnded().then(function (has) {
+         //     console.log("hasEnded: ", has);
+         //    });
+            });
+         }).catch(function(e) {
+           console.log("invalid buy?");
+         });
+       });
+  });
+});
